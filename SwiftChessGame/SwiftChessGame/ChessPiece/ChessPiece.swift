b3632c1e61822df773090615b4d15229a5409fe9
pingu.hwang <pingu.hwang@kakaocorp.com>
2022-06-23 13:47:20 +0900

    [Step-1] ChessPiece에 현재 이동가능한 위치 반환하는 기능 추가

--- SwiftChessGame/SwiftChessGame/ChessPiece/ChessPiece.swift
+++ SwiftChessGame/SwiftChessGame/ChessPiece/ChessPiece.swift
@@ -81,7 +81,85 @@ protocol ChessPiece {
 
 extension ChessPiece {
     var score: Int { pieceType.score }
-    var currentMoveableLocation: [Location] { currentLocation.moveAvailable(pieceType: pieceType) }
+    var currentMoveableLocationInfo: [String] { currentMoveableLocation.map { $0.string }.sorted() }
+    var currentMoveableLocation: [Location] {
+        var moveableLocation: [Location] = []
+        
+        switch pieceType {
+        case .queen:
+            moveableLocation += getAvailableStraigtMove(location: currentLocation)
+            moveableLocation += getAvailableDiagonalMove(location: currentLocation)
+        case .rook:
+            moveableLocation += getAvailableStraigtMove(location: currentLocation)
+        case .bishop:
+            moveableLocation += getAvailableDiagonalMove(location: currentLocation)
+        case .knight:
+            moveableLocation += getAvailableknightMove(location: currentLocation)
+        case .pawn:
+            moveableLocation += getAvailablePawnMove(location: currentLocation, color: teamColor)
+        }
+        return Array(Set(moveableLocation))
+    }
+    
+    private func getAvailableDiagonalMove(location: Location) -> [Location] {
+        let currentPoint = location.current
+        var returnValue: [Location] = []
+        let directions: [(ud: Int,lr: Int)] = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+        
+        for direction in directions {
+            for count in 1..<8 {
+                if let location = Location(col: currentPoint.file + count * direction.ud, row: currentPoint.rank + count * direction.lr) {
+                    returnValue.append(location)
+                } else {
+                    break
+                }
+            }
+        }
+
+        return returnValue
+    }
+    
+    private func getAvailableStraigtMove(location: Location) -> [Location] {
+        let currentPoint = location.current
+        var returnValue: [Location] = []
+        let directions: [(ud: Int, lr: Int)] = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        
+        for direction in directions {
+            for count in 1..<8 {
+                if let location = Location(col: currentPoint.file + count * direction.ud, row: currentPoint.rank + count * direction.lr) {
+                    returnValue.append(location)
+                } else {
+                    break
+                }
+            }
+        }
+        return returnValue
+    }
+    
+    private func getAvailableknightMove(location: Location) -> [Location] {
+        let currentPoint = location.current
+        var returnValue: [Location] = []
+        let directions: [(ud: Int, lr: Int)] = [(-2, -1), (-2, 1), (-1, 2), (1, 2), (2, -1), (2, 1), (-1, -2), (1, -2)]
+        
+        for direction in directions {
+            if let location = Location(col: currentPoint.file + direction.ud, row: currentPoint.rank + direction.lr) {
+                returnValue.append(location)
+            }
+        }
+        
+        return returnValue
+    }
+    
+    private func getAvailablePawnMove(location: Location, color: TeamColor) -> [Location] {
+        let currentPoint = location.current
+        
+        let newFile = currentPoint.file + (color == .black ? 1 : -1)
+        if let location = Location(col: newFile, row: currentPoint.rank) {
+            return [location]
+        }
+        
+        return []
+    }
 }
 
 struct ChessPieceFactory {
