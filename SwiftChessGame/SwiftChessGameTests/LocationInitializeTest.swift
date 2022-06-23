b92945c4a03130e66dd2db91d9b1c8d9d5307f24
pingu.hwang <pingu.hwang@kakaocorp.com>
2022-06-23 21:39:55 +0900

    [Step-1] Location 생성 테스트 작업

--- SwiftChessGame/SwiftChessGameTests/LocationInitializeTest.swift
+++ SwiftChessGame/SwiftChessGameTests/LocationInitializeTest.swift
@@ -9,5 +9,54 @@ import XCTest
 @testable import SwiftChessGame
 
 class LocationInitializeTest: XCTestCase {
-
+    func testCreateLocationWithInt() throws {
+        for file in 0..<8 {
+            for rank in 0..<8 {
+                let location = Location(col: file, row: rank)
+                XCTAssertNotNil(location)
+            }
+        }
+        
+        let location = Location(col: 8, row: 8)
+        XCTAssertNil(location)
+    }
+    
+    func testCreateLocationWithString() throws {
+        let aAsciiValue = Int(("A" as UnicodeScalar).value)
+        for file in 1...8 {
+            for rank in 0..<8 {
+                if let rankString = UnicodeScalar(aAsciiValue + rank) {
+                    let location = Location(string: "\(rankString)\(file)")
+                    XCTAssertNotNil(location)
+                }
+            }
+        }
+        
+        let location = Location(string: "Z9")
+        XCTAssertNil(location)
+    }
+    
+    func testCreateLocationWithTwoDifferentWayIsEqual() {
+        var createWithString: [Location?] = []
+        var createWithInt: [Location?] = []
+        let aAsciiValue = Int(("A" as UnicodeScalar).value)
+        
+        for file in 1...8 {
+            for rank in 0..<8 {
+                if let rankString = UnicodeScalar(aAsciiValue + rank) {
+                    let location = Location(string: "\(rankString)\(file)")
+                    createWithString.append(location)
+                }
+            }
+        }
+        
+        for file in 0..<8 {
+            for rank in 0..<8 {
+                let location = Location(col: file, row: rank)
+                createWithInt.append(location)
+            }
+        }
+        
+        XCTAssertEqual(createWithString, createWithInt)
+    }
 }
